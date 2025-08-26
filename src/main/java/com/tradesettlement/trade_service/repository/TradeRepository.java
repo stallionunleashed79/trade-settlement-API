302720c3139410c105c0b2607f06fc28f277b199
Bharath Seshadri <bharath.seshadri@photon.com>
2025-08-26 14:07:46 +0530

    Added code changes to persist trade status by trade id

--- src/main/java/com/tradesettlement/trade_service/repository/TradeRepository.java
+++ src/main/java/com/tradesettlement/trade_service/repository/TradeRepository.java
@@ -2,6 +2,15 @@ package com.tradesettlement.trade_service.repository;
 
 import com.tradesettlement.trade_service.entities.TradeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface TradeRepository extends JpaRepository<TradeEntity, Long> {
+
+    @Modifying
+    @Transactional // Required for modifying queries
+    @Query("UPDATE TradeEntity t SET t.status = :status WHERE t.tradeId = :tradeId")
+    int updateTradeStatusByTradeId(@Param("status") String status, @Param("tradeId") String tradeId);
 }
--- src/main/java/com/tradesettlement/trade_service/service/TradeStatusUpdateKafkaConsumer.java
+++ src/main/java/com/tradesettlement/trade_service/service/TradeStatusUpdateKafkaConsumer.java
@@ -4,6 +4,8 @@ import com.tradesettlement.trade_service.models.Trade;
 import com.tradesettlement.trade_service.models.TradeRequest;
 import com.tradesettlement.trade_service.models.TradeStatus;
 import com.tradesettlement.trade_service.models.TradeStatusUpdate;
+import com.tradesettlement.trade_service.repository.TradeRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -11,12 +13,14 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 
 @Component
+@RequiredArgsConstructor
 public class TradeStatusUpdateKafkaConsumer {
-    @Autowired
-    private StatusBroadcastService statusBroadcastService;
+
+    private final StatusBroadcastService statusBroadcastService;
+    private final TradeRepository tradeRepository;
 
     @KafkaListener(topics = "${settlement.events.topic}", groupId = "${settlement.events.group.id}")
-    public void handleStatusUpdate(TradeRequest tradeRequest) {
+    public void handleStatusUpdate(final TradeRequest tradeRequest) {
         final Trade trade = tradeRequest.getPayload();
         TradeStatusUpdate update = TradeStatusUpdate.builder()
                 .tradeId(trade.getTradeId())
@@ -24,6 +28,9 @@ public class TradeStatusUpdateKafkaConsumer {
                 .timestamp(LocalDateTime.now())
                 .correlationId(tradeRequest.getCorrelationId())
                 .build();
+        tradeRepository.updateTradeStatusByTradeId(trade.getStatus(),
+                trade.getTradeId());
+
         // Broadcast to all connected web clients
         statusBroadcastService.broadcastStatusUpdate(update);
     }
